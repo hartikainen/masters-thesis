a315fc48dc73836f8f1085e8542311eabe696d05
Kristian Hartikainen <kristian.hartikainen@aalto.fi>
2015-09-16 13:16:43 +0300

    Scheduler functions, Changed the simulation models

--- pse/custom/sso_scheduler.h
+++ pse/custom/sso_scheduler.h
@@ -1,3 +1,2 @@
-uint64_t CUSTOM_select(RNS_Resource *r, RNS_Client *queue, uint64_t waiting_size);
-uint64_t CUSTOM_reserve(RNS_Resource *r, uint64_t pc);
-uint64_t CUSTOM_release(RNS_Resource *r, uint64_t pc);
+uint64_t CUSTOM_select(RNS_Resource*, uint64_t);
+uint64_t CUSTOM_reserve(RNS_Resource*, uint64_t, RNS_Client**, uint64_t*, RNS_Queue_Attribute*);
